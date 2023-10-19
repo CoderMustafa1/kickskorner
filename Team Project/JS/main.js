18f5211bf7ec37ed46108dca088bb7ae3cd64618
CoderMustafa1 <147702825+CoderMustafa1@users.noreply.github.com>
2023-10-19 09:46:52 +0100

    Team project V.1 Homepage
    This commit is the homepage which serves as the base of the website

---
+++ Team Project/JS/main.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//   // Function to handle dropdown menus
+//   $('.underline').hover(function() {
+//       $(this).find('.dropdown-container').stop().slideDown(200);
+//   }, function() {
+//       $(this).find('.dropdown-container').stop().slideUp(200);
+//   });
+// });
