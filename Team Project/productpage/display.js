9c2ec9a7d8f1bab898a5e13931caf2f82ee301e9
Morayyyee <osibodu.momoore@gmail.com>
2023-11-30 09:32:08 +0000

    product page draft
    header has been left out

---
+++ Team Project/productpage/display.js
@@ -0,0 +1,29 @@
+//sidebar
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  //sort-by
+  function myFunction(){
+    document.getElementById().classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
+
