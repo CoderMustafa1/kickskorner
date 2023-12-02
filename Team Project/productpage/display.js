2768e18dffcb053a10bc0ea0094529eebec333d9
Morayyyee <osibodu.momoore@gmail.com>
2023-12-02 19:34:54 +0000

    testing the pop up function on the mens page
    pop up not fully functional, header to be changed

--- Team Project/productpage/display.js
+++ Team Project/productpage/display.js
@@ -1,29 +1,39 @@
 //sidebar
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
 
-  //sort-by
-  function myFunction(){
-    document.getElementById().classList.toggle("show");
-  }
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+//sort-by
+function myFunction() {
+  document.getElementById().classList.toggle("show");
+}
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
-  
+}
+
+//testing
+/*
+function openForm() {
+  document.getElementById("myPopup").style.display = "block";
+}
+
+/*function closeForm() {
+  document.getElementById("myPopup").style.display = "none";
+}
+*/
 
